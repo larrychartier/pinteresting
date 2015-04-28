8a0ceee54a4f2312f9a2b7339aeb54308fec0880
Larry Chartier <larrychartier@gmail.com>
2015-04-27 20:33:39 -0400

    Updating Pins Controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -38,7 +38,7 @@ class PinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions
     def set_pin
       @pin = Pin.find_by(id: params[:id])
     end
